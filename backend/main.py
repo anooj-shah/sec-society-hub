53863692b61b26c32fa1bddcda5778c315233a1f
Anooj Shah <thisisanooj@gmail.com>
2020-02-09 16:21:29 -0600

    Adding backend files

---
+++ backend/main.py
@@ -0,0 +1,119 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from pymongo import MongoClient
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# If modifying these scopes, delete the file token.pickle.
+@app.get("/")
+async def read_events():
+    """
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    service = setup_google_calendar()
+    dict = {}
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    i = 0
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+        dict[i] = (start, event['summary'])
+        i += 1
+    return dict
+
+@app.post("/")
+async def write_event(event):
+    # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+    # stored credentials.
+    # just for testing, but this would be read in from make_event()
+    service = setup_google_calendar()
+
+    event = {
+      'summary': 'Test Event 1',
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2020-02-09T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'end': {
+        'dateTime': '2020-02-09T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      # 'attendees': [
+      #   {'email': 'lpage@example.com'},
+      #   {'email': 'sbrin@example.com'},
+      # ],
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    s = 'Event created: %s' % (event.get('htmlLink'))
+    return s
+
+
+
+
+def setup_google_calendar():
+    # Set up Google Calendar API
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    return service
+
+def main():
+    service = setup_google_calendar()
+    read_events()
+    event = {}
+    write_event(event)
+    # client = MongoClient()
+    #
+    # client = MongoClient('mongodb://localhost:27017/')
+
+
+if __name__ == '__main__':
+    main()
