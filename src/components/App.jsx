263204c8f83be0ba874e0c70c8632b2a7ff2ec70
Shikhar Baheti <shikharbaheti@gmail.com>
2020-04-02 16:27:35 -0500

    Create MenuBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import "../styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row } from "react-bootstrap";
 
 import Header from "./Header";
-import CardDay from "./CardDay";
+import NavBar from "./NavBar";
+import DayView from "./DayView";
+import WeekView from "./WeekView";
+import MonthView from "./MonthView";
 
 function App() {
+  const [view, setView] = React.useState(WeekView);
+  function onDay() {
+    setView(DayView)
+  }
+
+  function onWeek() {
+    setView(WeekView)
+  }
+
+  function onMonth() {
+    setView(MonthView)
+  }
+
   return (
     <div>
       <Header />
-      <CardDay day="Monday" />
-      <CardDay day="Tuesday" />
-      <CardDay day="Wednesday" />
-      <CardDay day="Thursday" />
-      <CardDay day="Friday" />
+      <Container>
+        <Row>
+          <Col sm={{ size: 'auto', offset: 1 }}><NavBar day={onDay} week={onWeek} month={onMonth} /></Col>
+        </Row>
+        <Row>
+          <Col sm={"auto"}>{view}</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/WeekView.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import CardDay from "./CardDay";
+import { Container, Col, Row } from "react-bootstrap";
+
+function WeekView() {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col><CardDay day="Monday" agenda="SEC Meeting" time="8:15" /></Col>
+                    <Col><CardDay day="Tuesday" agenda="Big Event Meeting" time="8:15" /></Col>
+                    <Col><CardDay day="Wednesday" agenda="ZLP Meeting" time="8:15" /></Col>
+                    <Col><CardDay day="Thursday" agenda="Mechanical Meeting" time="8:15" /></Col>
+                    <Col><CardDay day="Friday" agenda="Computer Science Meeting" time="8:15" /></Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default WeekView;
